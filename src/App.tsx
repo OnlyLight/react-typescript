8a4fc20e3a5a700b75738855afa69d2a6913fd9d
tran.quang.duyb <tran.quang.duyb@i120936-mb.local>
2019-11-28 16:01:01 +0700

    ContextAPI & Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { TodoList } from "./components/TodoList";
+import { Store } from "./components/Reducer";
 import "./App.css";
 
-type typeElement = React.MouseEvent<HTMLButtonElement, MouseEvent>;
-
-interface ITodo {
-  text: string;
-  finish: boolean;
-}
-
 const App: React.FC = () => {
-  const [value, setValue] = useState<string>("");
-  const [todos, setTodos] = useState<ITodo[]>([]);
+  const { state, dispatch } = React.useContext(Store);
 
-  const handleSubmit = (e: typeElement): void => {
-    e.preventDefault();
-    setValue("");
-    addNewTodo(value);
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const addNewTodo = (text: string): void => {
-    const newTodo: ITodo[] = [...todos, { text: text, finish: false }];
-    setTodos(newTodo);
-  };
+  const fetchData = () => {
+    const result = {
+      episodes: ["zyz", "zyz", "zyz"],
+      favourites: ["abc", "abc", "abc"]
+    };
 
-  const completeTodo = (index: number): void => {
-    // const newTodo: ITodo[] = todos;
-    // newTodo[index].finish = !newTodo[index].finish;
-    setTodos([
-      ...todos.slice(0, index),
-      { text: todos[index].text, finish: !todos[index].finish },
-      ...todos.slice(index + 1, todos.length)
-    ]);
-  };
-
-  const deleteTodo = (index: number): void => {
-    setTodos([
-      ...todos.slice(0, index),
-      ...todos.slice(index + 1, todos.length)
-    ]);
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: result
+    });
   };
 
   return (
     <div className="App">
-      <h1>ToDo List</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={e => {
-          if (value) setValue(e.target.value);
-        }}
-        required
-      />
-      <button onClick={e => handleSubmit(e)}>Click to add !!</button>
-      <button
-        onClick={e => {
-          setTodos([]);
-          setValue("");
-        }}
-      >
-        Click to Clear !!
-      </button>
-      {todos.map((item: ITodo, index: number) => (
-        <div className="section-list" key={index}>
-          <div style={{ textDecoration: item.finish ? "line-through" : "" }}>
-            {item.text}
-          </div>
-          <button onClick={() => completeTodo(index)}>
-            {item.finish ? "UnFinish" : "Finish"}
-          </button>
-          <button onClick={() => deleteTodo(index)}>Delete</button>
-        </div>
-      ))}
+      <TodoList />
+      {console.log(state)}
     </div>
   );
 };
